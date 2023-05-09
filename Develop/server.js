191ca8fb510ea314e61b39969769335a951dded3
Mike Tsypan <miket@victorytransport.com>
2023-05-09 08:54:49 -0400

    few routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
+let db = require('./db/db.json')
+const fs = require("fs")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +11,20 @@ app.use(express.static("public"));
 
 app.get("/notes", (req, res)=> {
     console.log("test");
-    res.json({message: "testing"})
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
+app.get("/api/notes", (req, res)=> {
+    res.json(db)
+})
+
+app.post("/api/notes", (req, res)=> {
+    console.log(req.body)
+})
+
+app.get("*", (req, res)=> {
+    console.log("test");
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
 app.listen(PORT, ()=> console.log(`Listening on PORT: ${PORT}`));
