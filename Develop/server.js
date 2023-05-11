51a5ea740ec92b73b1b6d66f4d58cad7bb7fdced
Mike Tsypan <miket@victorytransport.com>
2023-05-11 07:22:32 -0400

    some changes

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -6,25 +7,38 @@ let db = require('./db/db.json')
 const fs = require("fs")
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/notes", (req, res)=> {
-    console.log("test");
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
-app.get("/api/notes", (req, res)=> {
+app.get("/api/notes", (req, res) => {
     res.json(db)
 })
 
-app.post("/api/notes", (req, res)=> {
-    console.log(req.body)
+app.post("/api/notes", (req, res) => {
+    // give an id to our note
+    req.body.id = uuidv4();
+    // add new note to our db variable
+    db.push(req.body);
+    console.log(db);
+    // with the fs module, we need to rewrite our file using the writeFile method
+    fs.writeFile('./db/db.json', JSON.stringify(db), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+    // send the new note back to the frontEnd
+    res.json(req.body)
 })
 
-app.get("*", (req, res)=> {
-    console.log("test");
+app.delete("/api/notes/:id", (req, res) => {
+    res.json(db)
+})
+
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
-app.listen(PORT, ()=> console.log(`Listening on PORT: ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
